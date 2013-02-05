a874aee2a1b693fc690d06f9dd5ce260d4f82815
menotti <menotti@4c8a2477-44d4-8db5-268b-d134fcd0ef5d>
2013-02-05 18:56:56 +0000

    Modificando exemplo estrutural

--- examples/figure15-structural/or4.vhd
+++ examples/figure15-structural/or4.vhd
@@ -2,11 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity or1 is
+entity or4 is
     port(a, b, c, d:in std_logic; y:out std_logic);
-end or1;
+end or4;
 
-architecture logica of or1 is
+architecture logica of or4 is
 begin
     y <= a or b or c or d;
 end logica;
