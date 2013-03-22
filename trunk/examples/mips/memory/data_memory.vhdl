8b6b4871d02ae0e3db9bd857c675da1cdf6dab2e
alangh.uabitapevi.si4@gmail.com <alangh.uabitapevi.si4@gmail.com@4c8a2477-44d4-8db5-268b-d134fcd0ef5d>
2013-03-22 18:40:30 +0000

    Removendo os arquivos obsoletos e adicionando os refatorados.

--- trunk/examples/mips/memory/data_memory.vhdl
+++ trunk/examples/mips/memory/data_memory.vhdl
@@ -2,23 +2,25 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
-use work.ufscar_memory_helpers.all;
 
-entity memory_block is
+entity data_memory is
 	generic (
+		length: integer := 256;
 		address_width: integer := 32;
-		predefined_data_set: memory_data_set := (others => (others => '0')));
+		data_width: integer := 32);
 
 	port (
 		address_to_read, address_to_write: in std_logic_vector (address_width - 1 downto 0);
 		data_to_write: in std_logic_vector (data_width - 1 downto 0);
 		read, write: in std_logic;
 		data_out: out std_logic_vector (data_width - 1 downto 0));
-end memory_block;
+end data_memory;
 
-architecture behavioral of memory_block is
+architecture behavioral of data_memory is
 
-	signal data_set: memory_data_set := predefined_data_set;
+	type data_sequence is array (0 to length - 1) of std_logic_vector (data_width - 1 downto 0);  
+	signal data: data_sequence :=
+		(others => (others => '0')); 
 
 begin
 
@@ -27,7 +29,7 @@ begin
 	begin
 		if read = '1' then
 			index := to_integer(unsigned(address_to_read));
-			data_out <= data_set(index);
+			data_out <= data(index);
 		end if;
 	end process;
 
@@ -36,7 +38,7 @@ begin
 	begin
 		if write = '1' then
 			index := to_integer(unsigned(address_to_write));
-			data_set(index) <= data_to_write;
+			data(index) <= data_to_write;
 		end if;
 	end process;	
 
