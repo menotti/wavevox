6e158223f32fee99572c382dfc965b6424e76cdf
alangh.uabitapevi.si4@gmail.com <alangh.uabitapevi.si4@gmail.com@4c8a2477-44d4-8db5-268b-d134fcd0ef5d>
2013-03-23 00:01:14 +0000

    Adicionando o projeto de um contador de programa simplificado.

---
+++ trunk/examples/mips/program_counter/program_counter.vhdl
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity program_counter is
+	generic (address_width: integer := 32);
+	port (
+		clock: in std_logic;
+		next_address: out std_logic_vector (address_width - 1 downto 0));
+end program_counter;
+
+architecture behavioral of program_counter is
+
+	signal current_address: std_logic_vector (address_width - 1 downto 0) := (others => '0');
+	signal maximum_address: std_logic_vector (address_width - 1 downto 0) := (others => '1'); 
+
+begin
+
+	next_address <= current_address;
+
+		process (clock)
+		begin
+			if rising_edge(clock) and current_address /= maximum_address then
+				current_address <= current_address + '1';
+			end if;
+		end process;
+
+end behavioral;
+
