b93463d2bc7472dbb93f2f5d7adee93344e701f3
menotti <menotti@4c8a2477-44d4-8db5-268b-d134fcd0ef5d>
2013-03-27 20:59:06 +0000

    Correcoes em componentes registrados

--- examples/mips/multi-cycle/componentes/program_counter/program_counter.vhdl
+++ examples/mips/multi-cycle/componentes/program_counter/program_counter.vhdl
@@ -5,7 +5,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity program_counter is
 	generic (address_width: integer := 32);
 	port (
-		clock, write_enable: in std_logic;
+		clock, enable: in std_logic;
 		next_address: out std_logic_vector (address_width - 1 downto 0));
 end program_counter;
 
@@ -18,11 +18,12 @@ begin
 
 	next_address <= current_address;
 
-		process (clock, write_enable)
+		process (clock, enable)
 		begin
-			if rising_edge(clock) and write_enable = '1' and 
-			current_address /= maximum_address then
-				current_address <= current_address + '1';
+			if rising_edge(clock) then
+				if enable = '1' and current_address /= maximum_address then
+					current_address <= current_address + '1';
+				end if;
 			end if;
 		end process;
 
