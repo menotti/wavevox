75c0db5ccd04bb94efdc7ec7f3fbd381d99e36ff
menotti <menotti@4c8a2477-44d4-8db5-268b-d134fcd0ef5d>
2013-04-30 13:36:34 +0000

    Exemplo em Verilog adicionado

---
+++ examples/verilog/dff/t_dff.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 100ps
+module t_dff;
+  reg d, clk;
+  wire q;
+
+  dff dut (d, clk, q);
+
+  initial begin
+    clk = 0;
+    d = 0;
+  end
+
+  always
+    #10 clk = !clk;
+
+  always @(negedge clk)
+    d = !d;
+
+  initial begin
+        #0 force q = 0;
+        #10 release q;
+	#1000 $finish;
+  end
+
+
+  initial  begin
+    $dumpfile ("t_dff.vcd"); 
+    $dumpvars; 
+  end 
+
+endmodule
